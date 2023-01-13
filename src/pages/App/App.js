72e2402f292ad151ff0dcf00921ceaf07a7bef0f
Riley Gramlich <gramlichriley@gmail.com>
2023-01-13 11:52:05 -0700

    set up client side routing rending pages (part 1 done)

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import React, { useState } from "react";
+
+import LoginPage from '../LoginPage/LoginPage';
+import MovieDetailPage from '../MovieDetailPage/MovieDetailPage';
+import MoviesListPage from '../MoviesListPage/MoviesListPage';
+import ActorListPage from '../ActorListPage/ActorListPage';
+import NavBar from '../../components/NavBar/NavBar';
+
 
 function App() {
+  const [user, setUser] = useState(true)
+
   return (
-    <div className="App">
-    </div>
+    <main className="App">
+      <h1 className="welcome">Welcome to React Movies!</h1>
+      {user ? (
+        <>
+          <NavBar/>
+          <Routes>
+            <Route path="/" element={<MoviesListPage />} />
+            <Route path="/movies/:movieName" element={<MovieDetailPage />} />
+            <Route path="/actors" element={<ActorListPage />} />
+          </Routes>
+        </>
+      ) : (
+        <LoginPage/>
+      )}
+    </main>
   );
 }
 
